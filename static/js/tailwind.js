0bf5d9f5e6f03469b9043155b7e190562c6cf3f4
TolyanL <timofey2007@gmail.com>
2025-10-02 17:23:27 +0300

    create base.html

---
+++ static/js/tailwind.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#6366f1',
+                secondary: '#8b5cf6',
+                dark: '#0f172a',
+                light: '#f8fafc'
+            }
+        }
+    }
+}
