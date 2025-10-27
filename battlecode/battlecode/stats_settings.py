0cdf2bae49c91bfac9c9f1906cce55d96791706f
Vovan <proboostvp@gmail.com>
2025-10-27 14:19:13 +0300

    correcting ranks

--- battlecode/battlecode/stats_settings.py
+++ battlecode/battlecode/stats_settings.py
@@ -1,14 +1,14 @@
 RANKS = (
-    ("1", "Lvl. 1 Rookie"),
-    ("2", "Lvl. 2 Recruit"),
-    ("3", "Lvl. 3 Apprentice"),
-    ("4", "Lvl. 4 Journeyman"),
-    ("5", "Lvl. 5 Adept"),
-    ("6", "Lvl. 6 Specialist"),
-    ("7", "Lvl. 7 Expert"),
-    ("8", "Lvl. 8 Champion"),
-    ("9", "Lvl. 9 Master"),
-    ("10", "Lvl. 10 Grandmaster"),
+    ("1", "Lvl. 1 Noobius I"),
+    ("2", "Lvl. 2 Noobius II"),
+    ("3", "Lvl. 3 Strategist I"),
+    ("4", "Lvl. 4 Strategist II"),
+    ("5", "Lvl. 5 Innovator I"),
+    ("6", "Lvl. 6 Innovator II"),
+    ("7", "Lvl. 7 Zero Bug I"),
+    ("8", "Lvl. 8 Zero Bug II"),
+    ("9", "Lvl. 9 Code Father I"),
+    ("10", "Lvl. 10 Code Gather II"),
 )
 
 RANKS_COLORS = [
