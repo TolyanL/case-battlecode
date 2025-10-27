d03b8f6d7f2de5df64ffd4bc2b356384a30ed02d
TolyanL <timofey2007l@gmail.com>
2025-10-27 12:40:54 +0300

    typo

--- battlecode/user/models.py
+++ battlecode/user/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import Group, User
-from django.db.models import Q, Count
+from django.db.models import Q
 from django.shortcuts import reverse
 
 from battlecode.stats_settings import RANKS, get_rank
